73dd9f598cbc6bc3efb35406ebc17993c55d9c70
bcrlyap <bcarlayap@ymail.com>
2013-02-16 09:23:00 +0800

    Update src/com/example/mapstutorial/MainActivity.java

--- src/com/example/mapstutorial/MainActivity.java
+++ src/com/example/mapstutorial/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity implements OnMapClickListener
   static final LatLng USCTC = new LatLng(10.35410, 123.91145);
   static final LatLng USCMC = new LatLng(10.30046, 123.88822);
 
-  private int checker = 0;
+  private int check = 0;
   private int maximum = 3;
   private GoogleMap map;
   
@@ -35,16 +35,12 @@ public class MainActivity extends Activity implements OnMapClickListener
 	    if(GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) == ConnectionResult.SUCCESS)
 	    {
 	    	map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-	    	    
-	    	if(a != null)
-	    	{
-	    		Marker usctc = map.addMarker(new MarkerOptions().position(USCTC).title("USC-TC"));
+	    	 	Marker usctc = map.addMarker(new MarkerOptions().position(USCTC).title("USC-TC"));
 	    		Marker uscmain = map.addMarker(new MarkerOptions().position(USCMC).title("USC-MAIN"));
-	    		Marker bronze = map.addMarker(new MarkerOptions().position(bronzeHorseman).title("Peter the Great in St. Petersburg"));
-	    	    	
+	    		Marker bronze = map.addMarker(new MarkerOptions().position(bronzeHorseman).title("Peter the Great in St. Petersburg"));	
 	    		map.setOnMapClickListener(this);
 	    		map.animateCamera(CameraUpdateFactory.newLatLngZoom(USCTC, 15), 2000, null);
-	    	}
+	    	
 	    }
   }
 
@@ -59,12 +55,12 @@ public class MainActivity extends Activity implements OnMapClickListener
 public void onMapClick(LatLng point) {
 	DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
 	    @Override
-	    public void onClick(DialogInterface dialog, int check) {
-	        switch (check)
+	    public void onClick(DialogInterface dialog, int i) {
+	        switch (i)
 	        {
 	        	case DialogInterface.BUTTON_POSITIVE:
 	            
-	        	switch(checker)
+	        	switch(check)
 	            {
 		            case 0:	map.animateCamera(CameraUpdateFactory.newLatLngZoom(USCTC, 15), 2000, null); 
 		            	break;
@@ -73,7 +69,7 @@ public void onMapClick(LatLng point) {
 		            case 2:	map.animateCamera(CameraUpdateFactory.newLatLngZoom(bronzeHorseman, 15), 2000, null); 
 	            }
 	            
-	            checker = (checker + 1) % maximum;
+	            check = (check + 1) % maximum;
 	            break;
 
 	        	case DialogInterface.BUTTON_NEGATIVE:
